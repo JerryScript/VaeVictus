0932ad2c9cd7feed28276dc4cc4f744d33684718
JerryScript <jerryscript@gmail.com>
2013-08-02 09:58:54 -0700

    Fix up the GPU frequency table

--- arch/arm/mach-msm/board-8960-gpu.c
+++ arch/arm/mach-msm/board-8960-gpu.c
@@ -37,26 +37,31 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
 			.gpu_freq = 400000000,
-			.bus_freq = 3,
+			.bus_freq = 4,
 			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 384000000,
+			.bus_freq = 3,
+			.io_fraction = 66,
 		},
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 2,
-			.io_fraction = 33,
-		},
-		{
-			.gpu_freq = 192000000,
-			.bus_freq = 1,
 			.io_fraction = 100,
 		},
 		{
-			.gpu_freq = 27000000,
+			.gpu_freq = 266667000,
+			.bus_freq = 1,
+			.io_fraction = 33,
+		},
+		{
+			.gpu_freq = 196667000,
 			.bus_freq = 0,
 		},
 	},
 	.init_level = 1,
-	.num_levels = 4,
+	.num_levels = 5,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
--- arch/arm/mach-msm/board-8960.c
+++ arch/arm/mach-msm/board-8960.c
@@ -2326,16 +2326,9 @@ static void __init msm8960_gfx_init(void)
 		kgsl_3d0_pdata->pwrlevel[4].gpu_freq = 196667000;
 #else
 				msm_kgsl_3d0.dev.platform_data;
-		kgsl_3d0_pdata->pwrlevel[2].gpu_freq = 320000000;
-		kgsl_3d0_pdata->pwrlevel[3].gpu_freq = 266667000;
+		kgsl_3d0_pdata->pwrlevel[0].gpu_freq = 320000000;
+		kgsl_3d0_pdata->pwrlevel[1].gpu_freq = 266667000;
 #endif
-	}
-}
-
-
-
-
-
 	}
 }
 
