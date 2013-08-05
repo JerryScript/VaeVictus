e9683200ebc1af1f29725fb82585c6621b77d4fc
JerryScript <jerryscript@gmail.com>
2013-08-04 19:33:18 -0700

    Fix typos in GPU overclock

--- arch/arm/mach-msm/board-gogh.c
+++ arch/arm/mach-msm/board-gogh.c
@@ -4091,7 +4091,7 @@ static void __init msm8960_gfx_init(void)
 		struct kgsl_device_platform_data *kgsl_3d0_pdata =
 #ifdef CONFIG_GPU_CONTROL
 				msm_kgsl_3d0.dev.platform_data;
-		kgsl_3d0_pdata->pwrlevel[0].gup_freq = 400000000;
+		kgsl_3d0_pdata->pwrlevel[0].gpu_freq = 400000000;
 		kgsl_3d0_pdata->pwrlevel[1].gpu_freq = 384000000;
 		kgsl_3d0_pdata->pwrlevel[2].gpu_freq = 320000000;
 		kgsl_3d0_pdata->pwrlevel[3].gpu_freq = 266667000;
