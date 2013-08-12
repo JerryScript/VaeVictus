037d7b979e7a957da84582d87cc57b344b9ccff9
Czar-Cortez Dixon <rbheromax@gmail.com>
2013-08-12 12:01:39 -0700

    Update board-8960-gpu.c

--- arch/arm/mach-msm/board-8960-gpu.c
+++ arch/arm/mach-msm/board-8960-gpu.c
@@ -36,24 +36,30 @@ static struct platform_device device_kgsl_3d0 = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
+			.gpu_freq = 500000000,
+			.bus_freq = 6,
+		},
+
+		{
+			.gpu_freq = 450000000,
+			.bus_freq = 5,
+		},
+		 
+		{
 			.gpu_freq = 400000000,
 			.bus_freq = 4,
-			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 325000000,
 			.bus_freq = 3,
-			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 300000000,
 			.bus_freq = 2,
-			.io_fraction = 66,
 		},
 		{
 			.gpu_freq = 200000000,
 			.bus_freq = 1,
-			.io_fraction = 100,
 		},
 		{
 			.gpu_freq = 128000000,
@@ -61,7 +67,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = 5,
+	.num_levels = 6,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
