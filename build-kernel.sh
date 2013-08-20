5ea0b9fabaa39dfdd47feed4542f04e1796eecbb
compsjp <compsjp@ubuntu.(none)>
2013-08-20 15:36:24 -0700

    fixed script

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,10 +1,10 @@
 #/bin/sh 
 
 #
-export dl-toolchains="git clone git://github.com/rbheromax/toolchains"
+export toolchains="git clone git://github.com/rbheromax/toolchains"
 export set="export CROSS_COMPILE=toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-"
-export J=$(cat /proc/cpuinfo | grep "^processor" | wc -l)
-
+J=$(cat /proc/cpuinfo | grep "^processor" | wc -l)
+clear
 echo "Preparing to build VaeVictus"
 echo " a kernel for the SPH-L300 goghvmu"
 echo " Virgin Mobile Galaxy Victory"
@@ -27,20 +27,20 @@ echo " "
 export user=$(whoami)
 sudo chown -R $user:$user *
 sudo chmod a+x -R *
-
+clear
 echo " "
 echo "Beginning actual build..."
 echo " "
 while true; do
-	read -p "Do you have a toolchain?"
+	read -p "Do you have a toolchain?" yn
 	case $yn in
-		[Yy]* ) break;;
-		[Nn]* ) $dl-toolchains; $set; break;;
+		[Yy]* ) echo ""; break;;
+		[Nn]* ) $toolchains; $set; break;;
 		*) echo "Gimme an answer dammit!!!"
 	esac
 done
 
-make ARCH=arm -j$(J)
+make ARCH=arm -j"$J"
 
 echo " "
 if [ -f arch/arm/boot/zImage ]
